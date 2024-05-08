8d450ff37e03a0f0d6f264df89fa17b6b439c8d6
prathapkammeta <94268573+prathapkammeta@users.noreply.github.com>
2024-05-08 20:27:22 +0530

    emoji-added-in-login-page

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
           ) : (
             "Login"
           )}
-          <span className=" text-blue-500 ">ChatApp</span>
+          <span className=" text-blue-500 ">Mern📲ChatApp</span>
         </h1>
         <form onSubmit={handleSubmit}>
           <div>
