0776d83b244f67cf4798c38050fb06d3485e9f8d
prathapkammeta <94268573+prathapkammeta@users.noreply.github.com>
2024-05-08 20:23:48 +0530

    bg-changed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import path from "path";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 app.get("/", (req, res) => {
   //root route http://localhost:5000/
   res.send("hellodd world");
@@ -20,6 +22,12 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+//express.static usually serve html css javascript files  in our frontend application
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
